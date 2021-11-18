1237d6cd983369aba26217f0c1a4d9f79705ee4d
AdviceDamy <79210960+AdviceDamy@users.noreply.github.com>
2021-11-18 16:17:10 +0100

    Update carousel

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,36 +1,72 @@
 import React from "react"
 // import Navigation from "../Navigation/Navigation"
-import Flickity from "react-flickity-component"
+import Flickity from "flickity"
 import CaseItem from "./views/case"
 
 // Import CSS
 import "./Slideshow.scss"
 
 const Slideshow = props => {
-  const flickityOptions = {
-    initialIndex: 1,
-    prevNextButtons: false,
-    pageDots: false,
-    wrapAround: true,
-    contain: true,
+  class Carousel extends React.Component {
+    componentDidMount = () => {
+      Flickity.createMethods.push("_createPrevNextCells")
+      Flickity.prototype._createPrevNextCells = function () {
+        this.on("select", this.setPrevNextCells)
+      }
+
+      Flickity.prototype.setPrevNextCells = function () {
+        // remove classes
+        changeSlideClasses(this.previousSlide, "remove", "is-previous")
+        changeSlideClasses(this.nextSlide, "remove", "is-next")
+        // set slides
+        this.previousSlide = this.slides[this.selectedIndex - 1]
+        this.nextSlide = this.slides[this.selectedIndex + 1]
+        // add classes
+        changeSlideClasses(this.previousSlide, "add", "is-previous")
+        changeSlideClasses(this.nextSlide, "add", "is-next")
+      }
+
+      function changeSlideClasses(slide, method, className) {
+        if (!slide) {
+          return
+        }
+        slide.getCellElements().forEach(function (cellElem) {
+          cellElem.classList[method](className)
+        })
+      }
+
+      var elem = document.querySelector(".carousel")
+      var flkty = new Flickity(elem, {
+        initialIndex: 1,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true,
+        contain: true,
+      })
+
+      console.log(flkty)
+    }
+
+    myCustomNext = () => {
+      // You can use Flickity API
+      this.flkty.next()
+    }
+
+    render() {
+      return (
+        <div class="carousel">
+          {props.items.map(post => {
+            return <CaseItem key={post.id} item={post} />
+          })}{" "}
+        </div>
+      )
+    }
   }
 
   return (
     <section className="slideshow">
       <div className="md:container">
-        <Flickity
-          className={"slideshow"} // default ''
-          elementType={"div"} // default 'div'
-          options={flickityOptions} // takes flickity options {}
-          disableImagesLoaded={true} // default false
-          reloadOnUpdate // default false
-          static // default false
-        >
-          {props.items &&
-            props.items.map(post => {
-              return <CaseItem item={post} />
-            })}{" "}
-        </Flickity>
+        <Carousel />
       </div>
     </section>
   )
