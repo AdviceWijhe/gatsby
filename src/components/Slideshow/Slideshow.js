5fa8c85858536fc5785a41264dd21bc403c2761d
AdviceDamy <79210960+AdviceDamy@users.noreply.github.com>
2021-11-19 08:46:10 +0100

    Slideshow change

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,65 +1,39 @@
 import React from "react"
 // import Navigation from "../Navigation/Navigation"
-import Flickity from "flickity"
+import { A11y } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
 import CaseItem from "./views/case"
 
 // Import CSS
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
 import "./Slideshow.scss"
 
 const Slideshow = props => {
-  class Carousel extends React.Component {
-    componentDidMount = () => {
-      Flickity.createMethods.push("_createPrevNextCells")
-      Flickity.prototype._createPrevNextCells = function () {
-        this.on("select", this.setPrevNextCells)
-      }
-
-      Flickity.prototype.setPrevNextCells = function () {
-        // remove classes
-        changeSlideClasses(this.previousSlide, "remove", "is-previous")
-        changeSlideClasses(this.nextSlide, "remove", "is-next")
-        // set slides
-        this.previousSlide = this.slides[this.selectedIndex - 1]
-        this.nextSlide = this.slides[this.selectedIndex + 1]
-        // add classes
-        changeSlideClasses(this.previousSlide, "add", "is-previous")
-        changeSlideClasses(this.nextSlide, "add", "is-next")
-      }
-
-      function changeSlideClasses(slide, method, className) {
-        if (!slide) {
-          return
-        }
-        slide.getCellElements().forEach(function (cellElem) {
-          cellElem.classList[method](className)
-        })
-      }
-
-      var elem = document.querySelector(".carousel")
-      new Flickity(elem, {
-        initialIndex: 1,
-        prevNextButtons: false,
-        pageDots: false,
-        wrapAround: true,
-        contain: true,
-      })
-    }
-
-    render() {
-      return (
-        <div class="carousel">
-          {props.items.map(post => {
-            return <CaseItem key={post.id} item={post} />
-          })}{" "}
-        </div>
-      )
-    }
-  }
-
   return (
     <section className="slideshow">
       <div className="md:container">
-        <Carousel />
+        <Swiper
+          modules={[A11y]}
+          spaceBetween={20}
+          slidesPerView={1.5}
+          centeredSlides={true}
+          loop={true}
+          onSwiper={swiper => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {" "}
+          {props.items &&
+            props.items.map(post => {
+              return (
+                <SwiperSlide>
+                  <CaseItem item={post}></CaseItem>
+                </SwiperSlide>
+              )
+            })}{" "}
+        </Swiper>
       </div>
     </section>
   )
--- src/components/Slideshow/views/case.js
+++ src/components/Slideshow/views/case.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
+import { SwiperSlide } from "swiper/react"
 // import Navigation from "../Navigation/Navigation"
 
 const CaseItem = props => {
-  console.log(props)
   return (
     <div key={props.item.title} className="slideshow__item">
       <div className="slideshow__item--inner">
