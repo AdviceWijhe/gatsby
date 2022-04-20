d2ab39be6063a569b8b75a9063e555d0eb4d2190
AdviceWijhe <49639728+AdviceWijhe@users.noreply.github.com>
2022-04-20 15:24:02 +0200

    flexible-content

---
+++ src/components/TwoColumnsContent/TwoColumnsContent.js
@@ -0,0 +1,44 @@
+// Import CSS
+import "./TwoColumnsContent.scss"
+
+import React from "react"
+import parse from "html-react-parser"
+
+// import Navigation from "../Navigation/Navigation"
+
+
+const TwoColumnsContent = props => {
+
+  return (
+    <section className={`TwoColumnsContent`}>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+           <div className="grid-1 pr-10">
+              <div className="grid__title">
+                {props.title1 &&
+                <h2 className="mt-0">{props.title1}</h2>
+                }
+              </div>
+              <div className="grid__content">
+                {props.content1 &&
+                parse(props.content1)
+                }
+              </div>
+           </div>
+           <div className="grid-2">
+               <div className="grid__title">
+                 {props.title2 &&
+                <h2 className="mt-0">{props.title2}</h2>
+                 }
+              </div>
+              <div className="grid__content">
+                {props.content2 &&
+                parse(props.content2)
+                }
+              </div>
+           </div>
+         </div>
+      </section>
+  )
+}
+
+export default TwoColumnsContent
